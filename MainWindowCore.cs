338d1a283e6f012b9bcb46ae75aec7706028b03b
baltasarq <baltasarq@gmail.com>
2015-10-09 15:22:28 +0200

    Added frames and boxes as viewers

--- MainWindowCore.cs
+++ MainWindowCore.cs
@@ -6,6 +6,19 @@ namespace gtksharp {
 			Gtk.Application.Quit();
 		}
 
+		private void OnShow() {
+			this.edText.Buffer.Text = @"Gtk#
+Is a graphic toolkit for user interfaces.
+It is quiet capable but also quite fixed in what to do and appearance.
+
+You can see in the source code how to create a main menu. More important,
+you can check out how to set actions and reuse them though all your code.
+
+Use View >> Boxes to see a dialog with boxes, and View >> Frames with frames.
+The main difference is that frames have a title, but you can still put a box inside.
+			";
+		}
+
 		private void OnAbout() {
 			var dlg = new Gtk.AboutDialog();
 
@@ -20,6 +33,68 @@ namespace gtksharp {
 			dlg.Copyright   = "(c) baltasar 2015";
 			dlg.Website     = "http://github.com/Baltasarq/GTKSharpDemo/";
 
+			dlg.Run();
+			dlg.Destroy();
+		}
+
+		private void OnViewFrames() {
+			var dlg = new Gtk.Dialog( "Frames", this, Gtk.DialogFlags.Modal );
+
+			var frame1 = new Gtk.Frame( "<b>Frame1</b>" );
+			( (Gtk.Label) frame1.LabelWidget ).UseMarkup = true;
+			frame1.Add( new Gtk.Label( "This is frame1" ) );
+			var frame2 = new Gtk.Frame( "<b>Frame2</b>" );
+			( (Gtk.Label) frame2.LabelWidget ).UseMarkup = true;
+			frame2.Add( new Gtk.Label( "This is frame2" ) );
+			var frame3 = new Gtk.Frame( "<b>Frame3</b>" );
+			( (Gtk.Label) frame3.LabelWidget ).UseMarkup = true;
+			frame3.Add( new Gtk.Label( "This is frame3" ) );
+
+
+			dlg.VBox.PackStart( frame1, true, true, 5 );
+			dlg.VBox.PackStart( frame2, true, true, 5 );
+			dlg.VBox.PackStart( frame3, true, true, 5 );
+
+			dlg.SetGeometryHints(
+				dlg,
+				new Gdk.Geometry() {
+					MinHeight = 200,
+					MinWidth = 320
+				},
+				Gdk.WindowHints.MinSize
+			);
+
+			dlg.AddButton( "Ok", Gtk.ResponseType.Ok );
+			dlg.ShowAll();
+			dlg.Run();
+			dlg.Destroy();
+		}
+
+		private void OnViewBoxes() {
+			var dlg = new Gtk.Dialog( "Boxes", this, Gtk.DialogFlags.Modal );
+
+			var hbBox1 = new Gtk.HBox( false, 5 );
+			hbBox1.Add( new Gtk.Label( "This is hbox 1" ) );
+			var hbBox2 = new Gtk.HBox( false, 5 );
+			hbBox2.Add( new Gtk.Label( "This is hbox 2" ) );
+			var hbBox3 = new Gtk.HBox( false, 5 );
+			hbBox3.Add( new Gtk.Label( "This is hbox 3" ) );
+
+			dlg.VBox.PackStart( hbBox1, true, true, 5 );
+			dlg.VBox.PackStart( hbBox2, true, true, 5 );
+			dlg.VBox.PackStart( hbBox3, true, true, 5 );
+
+			dlg.SetGeometryHints(
+				dlg,
+				new Gdk.Geometry() {
+					MinHeight = 200,
+					MinWidth = 320
+				},
+				Gdk.WindowHints.MinSize
+			);
+
+			dlg.AddButton( "Ok", Gtk.ResponseType.Ok );
+			dlg.ShowAll();
 			dlg.Run();
 			dlg.Destroy();
 		}
--- MainWindowView.cs
+++ MainWindowView.cs
@@ -13,22 +13,39 @@ namespace gtksharp {
 
 			this.BuildActions();
 			this.BuildMenu();
+			this.BuildTools();
 
 			// Events
 			this.DeleteEvent += (o, args) => this.OnClose();
+			this.Shown += (sender, e) => this.OnShow();
 
 			// Polish
 			this.WindowPosition = Gtk.WindowPosition.Center;
-			this.Add( vbMain );
-			this.Resize( 320, 200 );
+			this.Add( this.vbMain );
+			this.Resize( 640, 480 );
+
+			this.SetGeometryHints(
+				this,
+				new Gdk.Geometry() {
+					MinWidth = 320,
+					MinHeight = 200
+				},
+				Gdk.WindowHints.MinSize
+			); 
 		}
 
 		private void BuildActions() {
 			this.actQuit = new Gtk.Action( "Quit", "Quit", "Quit", Gtk.Stock.Quit );
 			this.actQuit.Activated += (o, evt) => this.OnClose();
 
-			this.actAbout = new Gtk.Action( "About...", "About", "About", Gtk.Stock.About );
+			this.actAbout = new Gtk.Action( "About...", "About...", "About...", Gtk.Stock.About );
 			this.actAbout.Activated += (o, evt) => this.OnAbout();
+
+			this.actViewFrames = new Gtk.Action( "ViewFrames", "Frames", "View frames", null );
+			this.actViewFrames.Activated += (obj, evt) => this.OnViewFrames();
+
+			this.actViewBoxes = new Gtk.Action( "ViewBoxes", "Boxes", "View boxes", null );
+			this.actViewBoxes.Activated += (obj, evt) => this.OnViewBoxes();
 		}
 
 		private void BuildMenu() {
@@ -37,20 +54,51 @@ namespace gtksharp {
 			var mFile = new Gtk.Menu();
 			var miHelp = new Gtk.MenuItem( "Help" );
 			var mHelp = new Gtk.Menu();
+			var miView = new Gtk.MenuItem( "View" );
+			var mView = new Gtk.Menu();
 
 			miFile.Submenu = mFile;
 			mFile.Append( this.actQuit.CreateMenuItem() );
-
 			miHelp.Submenu = mHelp;
 			mHelp.Append( this.actAbout.CreateMenuItem() );
+			miView.Submenu = mView;
+			mView.Append( this.actViewBoxes.CreateMenuItem() );
+			mView.Append( this.actViewFrames.CreateMenuItem() );
 
 			menuBar.Append( miFile );
+			menuBar.Append( miView );
 			menuBar.Append( miHelp );
 			this.vbMain.PackStart( menuBar, false, false, 5 );
 		}
 
-		private Gtk.VBox vbMain;
+		private void BuildTools() {
+			var hbBox = new Gtk.HBox( false, 5 );
+			var vbBox = new Gtk.VBox( false, 5 );
+
+			// Widgets
+			this.edText = new Gtk.TextView();
+			this.edText.Editable = false;
+
+			var btBoxes = new Gtk.Button( new Gtk.Label( this.actViewBoxes.Label ) );
+			btBoxes.Clicked += (sender, e) => this.OnViewBoxes();
+
+			var btFrames = new Gtk.Button( new Gtk.Label( this.actViewFrames.Label ) );
+			btFrames.Clicked += (sender, e) => this.OnViewFrames();
+
+			// Packing everything
+			vbBox.PackStart( btBoxes, false, false, 5 );
+			vbBox.PackStart( btFrames, false, false, 5 );
+			hbBox.PackStart( this.edText, true, true, 5 );
+			hbBox.PackStart( vbBox, false, false, 5 );
+			this.vbMain.PackStart( hbBox, true, true, 5 );
+		}
+
 		private Gtk.Action actQuit;
 		private Gtk.Action actAbout;
+		private Gtk.Action actViewFrames;
+		private Gtk.Action actViewBoxes;
+
+		private Gtk.VBox vbMain;
+		private Gtk.TextView edText;
 	}
 }
